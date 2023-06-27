07a8955a365e23177eaa268d7e760497f1932658
AnushkaNemishte <anushkanemishte36@gmail.com>
2023-06-27 17:39:38 +0530

    Imported some files

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "../node_modules/react-bootstrap/dist/react-bootstrap"
 import "../node_modules/bootstrap/dist/css/bootstrap.css"
-
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
