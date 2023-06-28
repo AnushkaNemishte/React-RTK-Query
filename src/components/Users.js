48f44c594badee8f7b1c77ae38a636b2e136a387
AnushkaNemishte <anushkanemishte36@gmail.com>
2023-06-28 23:13:52 +0530

    Table

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,33 +1,62 @@
 import React from 'react';
-import {Table} from 'react-bootstrap'
+import { Button, Col, Row, Table } from 'react-bootstrap'
 import { useGetUserQuery } from '../services/api';
+import { FaEye, FaTrash, FaPlus } from "react-icons/fa";
+
 function Users() {
-    const responseInfo = useGetUserQuery()
-    console.log(responseInfo)
+  const responseInfo = useGetUserQuery()
+  console.log(responseInfo)
   return (
-    <div className='container '>
+    <div className='container-fluid' >
+      <Row className='custom-padding center-items'>
+        <Col xs={8}>
+          <h1>Users Details</h1>
+        </Col>
+        <Col xs={2}>
+          <div className="input-group">
+            <input type="text" className="form-control" placeholder="Search..." />
+          </div>
+        </Col>
+        <Col xs={2}>
+          <div className="d-flex justify-content-end">
+            <Button variant='primary'><FaPlus />New Entry</Button>
+          </div>
+        </Col>
+      </Row>
       <Table striped bordered >
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Contact</th>
-            </tr>
+        <thead className='text-center'>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Contact</th>
+            <th>Action</th>
+            <th>Delete</th>
+          </tr>
         </thead>
-        <tbody>
-           {
-            responseInfo.data && responseInfo.data.map((items) =>{
-              return(
+        <tbody className='text-center'>
+          {
+            responseInfo.data && responseInfo.data.map((items) => {
+              return (
                 <tr key={items.id}>
-                    <th>{items.id}</th>
-                    <th>{items.name}</th>
-                    <th>{items.email}</th>
-                    <th>{items.contact}</th>
+                  <th>{items.id}</th>
+                  <th>{items.name}</th>
+                  <th>{items.email}</th>
+                  <th>{items.contact}</th>
+                  <th  >
+                    <Button variant='warning'>
+                      <FaEye />
+                      View</Button>
+                  </th>
+                  <th  >
+                    <Button variant='danger'>
+                      <FaTrash />
+                      Delete</Button>
+                  </th>
                 </tr>
               )
             })
-           }
+          }
         </tbody>
       </Table>
     </div>
