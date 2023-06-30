b27eb3552b612a012150ecfc01fe19ff5658dcbf
AnushkaNemishte <anushkanemishte36@gmail.com>
2023-06-30 23:05:48 +0530

    RTKQ CRUD

--- src/services/api.js
+++ src/services/api.js
@@ -5,14 +5,43 @@ export const api = createApi({
     baseQuery:fetchBaseQuery({
         baseUrl:"http://localhost:3001/"
     }),
+    tagTypes:["Users"],
     endpoints:(builder)=>({
         getUser:builder.query({
             query:()=>({
                 url:"users",
+                method:'GET',
+                providesTags: ['Users']
+            })
+        }),
+        getUserById:builder.query({
+            query:(id)=>({
+                url:`users/${id}`,
                 method:'GET'
+            })
+        }),
+        addUser:builder.mutation({
+            query:(newPost)=>({
+                url:`users`,
+                method:"POST",
+                body:newPost
+            }),
+            invalidatesTags:["Users"]
+        }),
+        updateUser:builder.mutation({
+            query:(updateUser)=>({
+                url:`users/${updateUser.id}`,
+                method:"PUT",
+                body:updateUser
+            })
+        }),
+        deleteUser:builder.mutation({
+            query:(id)=>({
+                url:`users/${id}`,
+                method:"DELETE"
             })
         })
     })
 })
 
-export const {useGetUserQuery}=api
+export const {useGetUserQuery,useAddUserMutation,useGetUserByIdQuery,useUpdateUserMutation,useDeleteUserMutation}=api
